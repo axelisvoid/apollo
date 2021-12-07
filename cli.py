8ecf754caeb00a3763146d301c92f530f690500f
Axel Rodríguez Chang <axelrodriguez@pm.me>
2021-12-07 09:15:04 +0100

    [cli.py]: Graciously handle keyboard interruption exceptions.

--- cli.py
+++ cli.py
@@ -1,22 +1,24 @@
 import subprocess as subp
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 
-def comm(cmd: str) -> Tuple[str, str]:
+def comm(cmd: str) -> Tuple[str, Optional[str]]:
     """Executes a shell command using `subp.Popen` interface."""
 
     with subp.Popen(f"{cmd}", shell=True, stdout=subp.PIPE) as proc:
-        stdout_, stderr_ = proc.communicate()
+        try:
+            stdout_, stderr_ = proc.communicate()
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt from KeyboardInterrupt
+        else:
+            dec_stdout = stdout_.decode()
 
-    dec_stdout = stdout_.decode()
     if stderr_:
         return dec_stdout, stderr_.decode()
-    else:
-        return dec_stdout, None
+    return dec_stdout, None
 
 
 def cmd_concat(cmds: List[str]) -> str:
     """Concatenates cli commands with the `&&` operator."""
 
     return " && ".join(cmds)
-
