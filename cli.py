1565635b4a55ac98edf5113bb219539659dcd609
Axel Rodríguez Chang <axelrodriguez@pm.me>
2021-12-11 08:46:14 +0100

    [cli.py]: Modified comm and added a function to handle apt cli warning.
    - comm function:
    	* Added a timeout for processes.
    	* Handle TimeoutExpired exceptions.
    
    - Added capture_and_remove_apt_warning function that eliminates apt's
      warnings from stderr.

--- cli.py
+++ cli.py
@@ -2,23 +2,37 @@ import subprocess as subp
 from typing import List, Optional, Tuple
 
 
-def comm(cmd: str) -> Tuple[str, Optional[str]]:
+def comm(cmd: str) -> Tuple[str, Optional[bytes]]:
     """Executes a shell command using `subp.Popen` interface."""
 
-    with subp.Popen(f"{cmd}", shell=True, stdout=subp.PIPE) as proc:
-        try:
-            stdout_, stderr_ = proc.communicate()
-        except KeyboardInterrupt:
-            raise KeyboardInterrupt from KeyboardInterrupt
-        else:
-            dec_stdout = stdout_.decode()
+    timeout = 15            # in seconds
 
-    if stderr_:
-        return dec_stdout, stderr_.decode()
-    return dec_stdout, None
+    with subp.Popen(f"{cmd}", shell=True, stdout=subp.PIPE, stderr=subp.PIPE) as proc:
+        try:
+            outs, errs = proc.communicate(timeout=timeout)
+        except TimeoutExpired:
+            proc.kill()
+            outs, errs = proc.communicate()
+        except KeyboardInterrupt:
+            proc.kill()
+            raise KeyboardInterrupt from KeyboardInterrupt
+
+    return outs, errs
 
 
 def cmd_concat(cmds: List[str]) -> str:
     """Concatenates cli commands with the `&&` operator."""
 
     return " && ".join(cmds)
+
+
+def capture_and_remove_apt_warning(errs: bytes) -> Optional[bytes]:
+    """Captures apt cli constant warning and removes it from an error bytes object."""
+
+    warning = b"\nWARNING: apt does not have a stable CLI interface. Use with caution in scripts.\n\n"
+
+    if warning in errs:
+        errs = errs[len(apt_err):]
+    if len(errs) == 0:
+        errs = None
+    return errs
