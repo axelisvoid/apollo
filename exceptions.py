88fe61d50f68f15d0c79bef5fd5bbf365ee92603
Axel Rodríguez Chang <axelrodriguez@pm.me>
2021-12-18 07:59:22 +0100

    [exceptions.py]: Added exceptions.

--- exceptions.py
+++ exceptions.py
@@ -1,4 +1,19 @@
+class CliError(Exception):
+    """Raised when a cli command throws an error."""
+    pass
+
+
+class ImgDownloadError(Exception):
+    """Raised when a specific image failed to download."""
+    pass
+
+
 class InstallationError(Exception):
     """Raised when some error ocurred during a program installation."""
+    pass
+
+
+class UnexistentPathError(Exception):
+    """Raised when a path to a specific directory does not exist."""
     pass
 
