979dd30bc11989049fabb2c7faf9ba69dc6d2230
Axel Rodríguez Chang <axelrodriguez@pm.me>
2021-12-20 07:44:31 +0100

    [post-installers.py]: Refactored post-installation functions to one individual file.

--- installers.py
+++ installers.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from shutil import copyfile, SameFileError, which
+from shutil import which
 from subprocess import TimeoutExpired
 from typing import List
 from cli import capture_and_remove_apt_warning, comm
@@ -148,15 +148,15 @@ def install_brave_browser() -> bool:
            "tee /etc/apt/sources.list.d/brave-browser-release.list"
           )
 
-    print("Adding Brave Browser's Apt repository...")
+    print("Adding Brave Browser's apt repository...")
     _, errs = comm(cmd)
     if errs:
         raise InstallationError("Failed to add Brave Browser's ppa.")
-    print("Successfully added Brave Browser's Apt repository.")
+    print("Successfully added Brave Browser's apt repository.")
 
     cmd = "apt update -y && apt install -y brave-browser"
 
-    print("Installing Brave Browser's apt package...")
+    print("Installing brave-browser...")
     _, errs_ = comm(cmd)
     if errs_:
         errs = capture_and_remove_apt_warning(errs_)
@@ -182,8 +182,7 @@ def install_docker() -> bool:
     _, errs = comm(cmd)
     if errs:
         raise InstallationError("Failed to add Docker's fingerprint.")
-    print("Successfully added signing keys.")
-
+    print("Successfully added Docker's signing keys.")
 
     cmd = ("echo "
            "\"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] "
@@ -197,9 +196,9 @@ def install_docker() -> bool:
     _, errs = comm(cmd)
     if errs:
         raise InstallationError("Failed to add Docker's ppa.")
-    print("Apt repository successfully added.")
+    print("Docker's apt repository successfully added.")
 
-    print("Installing all necessary apt packages...")
+    print("Updating apt repositories...")
     cmd = "apt update -y"
     _, errs_ = comm(cmd)
     if errs_:
@@ -209,12 +208,12 @@ def install_docker() -> bool:
 
     pkgs = ["docker-ce", "docker-ce-cli", "containerd.io"]
     for pkg in pkgs:
-        print(f"Installing {pkg}")
+        print(f"Installing {pkg}...")
         _, errs_ = comm(f"apt install -y {pkg}")
         if errs_:
             errs = capture_and_remove_apt_warning(errs_)
             if errs:
-                raise InstallationError("Failed to install Docker apt packages.")
+                raise InstallationError(f"Failed to install Docker {pkg}.")
     print("Installation successful.")
 
     # post-install step required for all Linux distros
@@ -223,6 +222,7 @@ def install_docker() -> bool:
     print("Adding Docker group and adding user to it...")
     _, errs = comm(cmd)
     if errs:
+        print(errs)
         raise InstallationError("Failed to add Docker's group and/or add user to Docker's group.")
     print("Successfully added user to Docker group")
 
@@ -256,7 +256,7 @@ def install_fish_shell() -> bool:
     print("Fingerprint successfully added.")
 
     cmd = "apt update -y && install -y fish"
-    print("Installing Fish shell's apt package...")
+    print("Installing fish...")
     _, errs_ = comm(cmd)
     if errs_:
         errs = capture_and_remove_apt_warning(errs_)
@@ -386,111 +386,6 @@ def install_not_ppkd_prog() -> bool:
     except (InstallationError, TimeoutExpired):
         raise InstallationError("Poetry was not installed.")
     print("Successfully installed Poetry.")
-
-    return True
-
-
-############################################ posts ###############################
-
-
-def post_fish_shell() -> bool:
-    """Sets fish as the default shell and copies fish functions to their configuration directory."""
-
-    cmd = "-u $USER chsh -s `which fish`"
-    _, errs = comm(cmd)
-    if errs:
-        raise InstallationError("Fish shell's post installation procedure failed.")
-
-    config_dest = f"{HOME_PATH}/.config/fish/functions"
-    files = ["fish_greeting.fish", "fish_prompt.fish"]
-    for file in files:
-        src = f"{CONFIG_FILES_PATH}/fish/{file}"
-        try:
-            copyfile(src, f"{config_dest}/{file}")
-        except SameFileError:
-            raise SameFileError("The source and destination files are the same.") from SameFileError
-
-    return True
-
-
-def post_neovim() -> bool:
-    """Sets up vim alias for nvim and copies `init.vim` file to configuration directory."""
-
-    file = "nvim.appimage"
-    src = f"{DOWNLOADS_PATH}/{file}"
-    dest = f"/usr/local/bin/{file}"
-
-    try:
-        copyfile(src, dest)
-    except SameFileError:
-        raise SameFileError("The source and destination files are the same.")
-
-    # make vim command execute nvim
-    cmd = "update-alternatives --install /usr/bin/vim vim {dest} 110"
-    _, errs = comm(cmd)
-    if errs:
-        raise InstallationError("Failed to create vim alias for neovim.")
-
-    # copy `init.vim` to conf directory
-
-    cmd = "mkdir ~/.config/neovim"
-    _, errs = comm(cmd)
-    if errs:
-        raise InstallationError("Failed to create neovim config directory.")
-
-    src = f"{CONFIG_FILES_PATH}/neovim/init.vim"
-    dest = f"~/.config/neovim/init.vim"
-    try:
-        copyfile(src, dest)
-    except SameFileError:
-        raise SameFileError("The source and destination files are the same.")
-
-    return True
-
-
-def post_tmux() -> bool:
-    """Fetches Tmux Plugin Manager and copies `.tmux.conf` file to `HOME_PATH`."""
-
-    tpm_repo = "https://github.com/tmux-plugins/tpm"        # tpm repo url
-    tpm_clone_path = "~/.tmux/plugins/tpm"                  # tpm repo dest path
-
-    # clone Tmux Plugin Manager github repo
-    cmd = f"git clone {tpm_repo} {tpm_clone_path}"
-    _, errs = comm(cmd)
-    if errs:
-        raise InstallationError("Failed to clone Tmux Plugin Manager's github repo.")
-
-    # copy .tmux.conf file
-    src = f"{CONFIG_FILES_PATH}/tmux.conf"                  # tmux.conf source path
-    dest = "~/.tmux.conf"                                   # tmux.conf dest path
-    try:
-        copyfile(src, dest)
-    except SameFileError:
-        raise SameFileError("The source and destination files are the same.") from SameFileError
-
-    return True
-
-
-def post_install() -> bool:
-    """Procedures that should occur after all programs have been installed."""
-
-    print("Starting post-installation procedures for Fish shell.")
-    try:
-        post_fish_shell()
-    except InstallationError:
-        raise InstallationError("Fish shell's post installation procedures failed.")
-
-    print("Starting post-installation procedures for Neovim.")
-    try:
-        post_neovim()
-    except InstallationError:
-        raise InstallationError("Neovim's post installation procedures failed.")
-
-    print("Starting post-installation procedures for Tmux.")
-    try:
-        post_tmux()
-    except InstallationError:
-        raise InstallationError("Tmux's post installation procedures failed.")
 
     return True
 
