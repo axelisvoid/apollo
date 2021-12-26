0e9f573c6c32878e3b5a8d50d51a0f594398aa4d
Axel Rodríguez Chang <axelrodriguez@pm.me>
2021-12-26 21:56:32 +0100

    [cli.py]: Fixed function call without first argument.

--- cli.py
+++ cli.py
@@ -34,7 +34,7 @@ def car_expected_err_msg(msg: bytes, errs: bytes) -> Optional[bytes]:
         tmp_subs = s_errs[res[0]:res[1]]
         s_errs = s_errs.replace(tmp_subs, "")
         errs_ = bytes(s_errs, "utf8")
-        errs = car_expected_err_msg(errs_)
+        errs = car_expected_err_msg(msg, errs_)
     
     return errs
 
