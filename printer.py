c5bee9ebfe1f1bc4e38d002c5ffcccf24547f3c4
Axel Rodríguez Chang <axelrodriguez@pm.me>
2022-05-19 20:21:37 +0200

    Remove old Apollo files.

--- printer.py
+++
@@ -1,8 +0,0 @@
-RED   = "\033[0;31m"
-GREEN = "\033[0;32m"
-RESET = "\033[0m"
-
-
-print_err = lambda msg: print(RED, f"[ ERROR ]: {msg}", RESET)       # Prints a message to the terminal as an error message.
-print_suc = lambda msg: print(GREEN, f"[ SUCCESS ]: {msg}", RESET)   # Prints a message to the terminal as a success message.
-
